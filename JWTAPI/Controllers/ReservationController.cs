90c1a74a0f2352b3173d542afa25aac25eb229ba
sunil kumar <sunil.pro@hotmail.com>
2020-08-25 14:59:57 +0530

    Add project files.

---
+++ JWTAPI/Controllers/ReservationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JWTAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ReservationController : ControllerBase
+    {
+        //https://www.yogihosting.com/jwt-api-aspnet-core/#download
+        //[HttpGet]
+        //public IEnumerable<Reservation> Get() => CreateDummyReservations();
+        [HttpGet]
+        public IEnumerable<Reservation> Get()
+        {
+            var claims = HttpContext.User.Claims;
+            return CreateDummyReservations().Where(t => t.Name == claims.FirstOrDefault(c => c.Type == "Name").Value);
+        }
+
+        public List<Reservation> CreateDummyReservations()
+        {
+            List<Reservation> rList = new List<Reservation> {
+            new Reservation {Id=1, Name = "Ankit", StartLocation = "New York", EndLocation="Beijing" },
+            new Reservation {Id=2, Name = "Bobby", StartLocation = "New Jersey", EndLocation="Boston" },
+            new Reservation {Id=3, Name = "Jacky", StartLocation = "London", EndLocation="Paris" }
+            };
+            return rList;
+        }
+    }
+}
---
+++ JWTClient/Controllers/CallAPIController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace JWTClient.Controllers
+{
+    public class CallAPIController : Controller
+    {
+        //https://www.yogihosting.com/jwt-api-aspnet-core/#download
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(string username, string password)
+        {
+            if ((username != "Secret") || (password != "Secret"))
+                return View((object)"Wrong Username or Password");
+
+            var tokenString = GenerateJSONWebToken();
+            List<Reservation> reservationList = new List<Reservation>();
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+                using (var response = await httpClient.GetAsync("https://localhost:44359/Reservation"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<Reservation>>(apiResponse);
+                }
+            }
+
+            return View("Reservation", reservationList);
+        }
+
+        private string GenerateJSONWebToken()
+        {
+            var claims = new[] {
+                                new Claim("Name", "Bobby"),
+                                new Claim(JwtRegisteredClaimNames.Email, "hello@yogihosting.com"),
+                                };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MynameisJamesBond007"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "https://www.yogihosting.com",
+                audience: "https://www.yogihosting.com",
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: credentials,
+                claims: claims  
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
